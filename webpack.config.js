f3dbb84ad7e619a2912e6fb2ee827ab33e4dcad0
zack9433 <zack9433@gmail.com>
2016-03-20 11:26:17 +0800

    remove useless loader task

--- webpack.config.js
+++ webpack.config.js
@@ -30,16 +30,6 @@ var config = {
     ],
     loaders: [
       {test: /\.js$/, loader: 'ng-annotate!babel', exclude: /(node_modules)/},
-      {
-        test: /\.js$/,
-        loader: 'babel?optional[]=runtime&stage=0',
-        include: /(angular-sanji-window)/
-      },
-      {
-        test: /\.html$/,
-        loader: 'ng-cache?prefix=[dir]/[dir]',
-        include: /(angular-sanji-window)/
-      },
       {test: /\.html$/, loader: 'ng-cache?prefix=[dir]/[dir]', exclude: /(node_modules)/}
     ],
     noParse: []
